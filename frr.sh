8ac381c923809c7b6017f29298d37da4ff53e6b5
GINZYHIT <ginzy.hit@gmail.com>
2024-10-22 11:39:47 +0300

    Update frr.sh

--- frr.sh
+++ frr.sh
@@ -1,15 +1,15 @@
 vtysh
-  configure terminal
-    interface tun0
-      ip ospf authentication message-digest
-      ip ospf message-digest-key 1 md5 P@ssw0rd
-      exit
-    router ospf
-      network 192.168.100.0/26 area 0
-      network 192.168.200.0/28 area 0
-      network 192.168.99.0/29 area 0
-      network 172.16.30.0/30 area 0
-      do wr
-      exit
-    exit
-  exit
+vtysh configure terminal
+vtysh interface tun0
+vtysh ip ospf authentication message-digest
+vtysh ip ospf message-digest-key 1 md5 P@ssw0rd
+vtysh exit
+vtysh router ospf
+vtysh network 192.168.100.0/26 area 0
+vtysh network 192.168.200.0/28 area 0
+vtysh network 192.168.99.0/29 area 0
+vtysh network 172.16.30.0/30 area 0
+vtysh do wr
+vtysh exit
+vtysh exit
+vtysh exit
