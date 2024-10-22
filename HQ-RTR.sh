3102689874f6a95ca981d9a53f15487413173cd2
GINZYHIT <ginzy.hit@gmail.com>
2024-10-22 10:34:51 +0300

    Update HQ-RTR.sh

--- HQ-RTR.sh
+++ HQ-RTR.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 mkdir /etc/net/ifaces/enp6s{19.100,19.200,19.999}
 mkdir /etc/net/ifaces/tun0
+echo 172.16.4.2/28 > /etc/net/ifaces/enp6s18/ipv4address
+echo default via 172.16.4.1/28 > /etc/net/ifaces/enp6s18/ipv4route
+echo nameserver 77.88.8.8 > /etc/net/ifaces/enp6s18/resolv.conf
+echo 192.168.100.1/26 > /etc/net/ifaces/enp6s19.100/ipv4address
+echo 192.168.200.1/28 > /etc/net/ifaces/enp6s19.200/ipv4address
+echo 192.168.99.1/29 > /etc/net/ifaces/enp6s19.999/ipv4address
+echo 172.16.30.1/30 > /etc/net/ifaces/tun0/ipv4address
 echo -e "TYPE=vlan\nHOST=enp6s19\nVID=100\nBOOTPROTO=static" > /etc/net/ifaces/enp6s19.100/options
 echo -e "TYPE=vlan\nHOST=enp6s19\nVID=200\nBOOTPROTO=static" > /etc/net/ifaces/enp6s19.200/options
 echo -e "TYPE=vlan\nHOST=enp6s19\nVID=999\nBOOTPROTO=static" > /etc/net/ifaces/enp6s19.999/options
@@ -25,13 +32,6 @@ network 172.16.30.0/30 area 0
 do wr
 iptables -t nat -A POSTROUTING -o enp6s18 -j MASQUERADE
 iptables-save > /etc/sysconfig/iptables
-echo 172.16.4.2/28 > /etc/net/ifaces/enp6s18/ipv4address
-echo default via 172.16.4.1/28 > /etc/net/ifaces/enp6s18/ipv4route
-echo nameserver 77.88.8.8 > /etc/net/ifaces/enp6s18/resolv.conf
-echo 192.168.100.1/26 > /etc/net/ifaces/enp6s19.100/ipv4address
-echo 192.168.200.1/28 > /etc/net/ifaces/enp6s19.200/ipv4address
-echo 192.168.99.1/29 > /etc/net/ifaces/enp6s19.999/ipv4address
-echo 172.16.30.1/30 > /etc/net/ifaces/tun0/ipv4address
 hostnamectl set-hostname isphq-rtr.au-team.irpo
 echo propishi ety commandy systemctl enable --now iptables
 
