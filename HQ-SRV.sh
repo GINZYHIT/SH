3e19da02a2eab4d932d073d2ebddd31ef25a31f8
GINZYHIT <ginzy.hit@gmail.com>
2024-10-23 09:02:07 +0300

    Update HQ-SRV.sh

--- HQ-SRV.sh
+++ HQ-SRV.sh
@@ -1,19 +1,15 @@
 #!/bin/bash
 hostnamectl set-hostname hq-srv.au-team.irpo
-mkdir /etc/net/ifaces/enp6s{19,19.100,19.200,19.999}
-mkdir /etc/net/ifaces/tun0
-cp /etc/net/ifaces/enp6s18/options /etc/net/ifaces/enp6s19/
-echo 172.16.4.2/28 > /etc/net/ifaces/enp6s18/ipv4address
-echo default via 172.16.4.1 > /etc/net/ifaces/enp6s18/ipv4route
+echo 192.168.100.2/26 > /etc/net/ifaces/enp6s18/ipv4address
+echo default via 192.168.100.1 > /etc/net/ifaces/enp6s18/ipv4route
 echo nameserver 77.88.8.8 > /etc/net/ifaces/enp6s18/resolv.conf
-echo 192.168.100.1/26 > /etc/net/ifaces/enp6s19.100/ipv4address
-echo 192.168.200.1/28 > /etc/net/ifaces/enp6s19.200/ipv4address
-echo 192.168.99.1/29 > /etc/net/ifaces/enp6s19.999/ipv4address
-echo 172.16.30.1/30 > /etc/net/ifaces/tun0/ipv4address
-echo -e "TYPE=vlan\nHOST=enp6s19\nVID=100\nBOOTPROTO=static" > /etc/net/ifaces/enp6s19.100/options
-echo -e "TYPE=vlan\nHOST=enp6s19\nVID=200\nBOOTPROTO=static" > /etc/net/ifaces/enp6s19.200/options
-echo -e "TYPE=vlan\nHOST=enp6s19\nVID=999\nBOOTPROTO=static" > /etc/net/ifaces/enp6s19.999/options
-echo -e "TYPE=iptun\nTUNTYPE=gre\nTUNLOCAL=172.16.4.2\nTUNREMOTE=172.16.5.2\nTUNTTL=64\nTUNMTU=1400\nTUNOPTIONS='ttl 64'" > /etc/net/ifaces/tun0/options
+systemctl restart network
+useradd sshuser -u 1010
+passwd sshuser
+P@ssw0rd
+P@ssw0rd
+usermod -aG wheel sshuser
+
 cp /etc/net/sysctl.conf /etc/net/sysctl.conf.bak
 sed -i 's/net.ipv4.ip_forward = 0/net.ipv4.ip_forward = 1/g' /etc/net/sysctl.conf
 systemctl restart network
