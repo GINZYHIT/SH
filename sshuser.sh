26c23a1e6600251a24922bb4da457e5e9dbde8e6
GINZYHIT <ginzy.hit@gmail.com>
2024-10-23 09:21:07 +0300

    Update sshuser.sh

--- sshuser.sh
+++ sshuser.sh
@@ -1,1 +1,15 @@
+#!/bin/bash
 
+read -p "sshuser" username
+read -s -p "P@ssw0rd $username: " password
+echo ""
+
+if id "$username" &> /dev/null; then
+  echo "Пользователь с таким именем уже существует."
+  exit 1
+fi
+
+useradd "$username"
+echo "$password" | passwd --stdin "$username"
+
+echo "Пользователь $username создан."
