8cc6a4510d81aeea0dd81e5f352bce014e65c1e1
GINZYHIT <ginzy.hit@gmail.com>
2024-10-23 18:02:02 +0300

    Update bind.sh

--- bind.sh
+++ bind.sh
@@ -3,3 +3,4 @@ apt-get update
 apt-get install bind bind-utils -y
 echo nameserver 127.0.0.1 > /etc/net/ifaces/enp6s18/resolv.conf
 systemctl restart network
+mv -f SH/options.conf /etc/bind/options.conf
