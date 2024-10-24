fb94c9af5b98a575c002df073e49c629412fc3df
GINZYHIT <ginzy.hit@gmail.com>
2024-10-24 12:31:36 +0300

    Update ISP.sh

--- ISP.sh
+++ ISP.sh
@@ -18,13 +18,6 @@ echo 172.16.5.1/28 > /etc/net/ifaces/enp6s20/ipv4address
 hostnamectl set-hostname isp.au-team.irpo
 echo propishi ety commandy systemctl enable --now iptables
 systemctl restart network
-
-chmod 777 SH/
-apt-get install sudo -y
-echo -e "WHEEL_USERS ALL=(ALL:ALL) ALL\nWHEEL_USERS ALL=(ALL:ALL) NOPASSWD: ALL" >> /etc/sudoers
-sh SH/1.sh
-scp -r /root/SH root@172.16.4.2:/root/
-
 apt-get remove git -y
 history -c
 rm -rf SH
