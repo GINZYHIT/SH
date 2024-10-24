f5a43dd7901cc7dd76335b33d46363e98a22b31d
GINZYHIT <ginzy.hit@gmail.com>
2024-10-24 19:54:27 +0300

    Update HQ-RTR.sh

--- HQ-RTR.sh
+++ HQ-RTR.sh
@@ -30,6 +30,6 @@ mv -f SH/dhcpd.conf /etc/dhcp/dhcpd.conf
 sed -i 's/DHCPDARGS=/DHCPDARGS=enp6s19.200/g' /etc/sysconfig/dhcpd
 systemctl restart dhcpd
 #apt-get remove git -y
-rm -rf /home/user/SH
+rm -rf SH
 clear
 exec bash
