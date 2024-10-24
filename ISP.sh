2c9e57dcec225f2443dc687d838001696d754520
GINZYHIT <ginzy.hit@gmail.com>
2024-10-24 12:59:45 +0300

    Update ISP.sh

--- ISP.sh
+++ ISP.sh
@@ -18,8 +18,10 @@ echo 172.16.5.1/28 > /etc/net/ifaces/enp6s20/ipv4address
 hostnamectl set-hostname isp.au-team.irpo
 echo propishi ety commandy systemctl enable --now iptables
 systemctl restart network
+
 sh SH/1.sh
 scp -r /root/SH/* user@172.16.4.2:/home/user/SH
+
 apt-get remove git -y
 history -c
 rm -rf SH
