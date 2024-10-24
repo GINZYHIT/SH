e66ae0099b1e8c8f8279817ba19e5f210cd6a01b
GINZYHIT <ginzy.hit@gmail.com>
2024-10-24 11:37:22 +0300

    Update D.sh

--- D.sh
+++ D.sh
@@ -1,1 +1,10 @@
+#!/bin/bash
+mac_address="XX:XX:XX:XX:XX:XX"
+while true; do
+  if arp -a | grep "$mac_address" > /dev/null; then
+    echo "MAC-адрес найден, запуск скрипта..."
+    SH/ISP.sh
+  fi
+  sleep 10 # Проверка каждые 10 секунд
+done
 
