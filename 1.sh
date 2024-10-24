dd81359b5eeb410676cc32cdf642fd7245bd3b9e
GINZYHIT <ginzy.hit@gmail.com>
2024-10-24 12:10:30 +0300

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,3 +1,19 @@
-#!bin/bash
-sh SH/ISP.sh
+#!/bin/bash
+key_path="$HOME/.ssh/id_ed25519"  # Путь к ключу
+key_size=2048 # Размер ключа
 
+# Удаление старых ключей (будьте осторожны!)
+rm -f "$key_path" "$key_path.pub"
+
+# Генерация ключа без пароля
+ssh-keygen -t rsa -b "$key_size" -f "$key_path" -N "" > /dev/null 2>&1
+
+echo "Ключ SSH сгенерирован в $key_path и $key_path.pub"
+echo "Внимание! Ключ сгенерирован без пароля, это снижает безопасность!"
+
+# Копирование публичного ключа на сервер (НЕОБЯЗАТЕЛЬНО, требует ssh-copy-id)
+
+#Введите имя пользователя и IP сервера
+read -p "root@172.16.4.2" user_server
+
+ssh-copy-id $user_server
