66ffe6d0616c320eb1df5d52700e86668dd84063
GINZYHIT <ginzy.hit@gmail.com>
2024-10-31 10:48:12 +0300

    Update HQ-RTR.sh

--- HQ-RTR.sh
+++ HQ-RTR.sh
@@ -31,9 +31,9 @@ mv -f SH/dhcpd.conf /etc/dhcp/dhcpd.conf
 sed -i 's/DHCPDARGS=/DHCPDARGS=enp6s19.200/g' /etc/sysconfig/dhcpd
 systemctl restart dhcpd
 #apt-get remove git -y
-#echo VVEDY YES I NAPISHY PAROL 'resu'
-#sudo scp -r /home/user/SH/* user@192.168.100.2:/home/user/SH
-
+echo VVEDY YES I NAPISHY PAROL 'resu'
+scp -r /home/user/SH/* user@192.168.100.2:/home/user/SH
+ssh user@192.168.100.2 "sudo bash /home/user/SH/HQ-SRV.sh"
 #rm -rf SH
 history -c
 clear
