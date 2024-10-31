2fbbe2cba5ecc244f9cf8b05d6d29786d50f02dc
GINZYHIT <ginzy.hit@gmail.com>
2024-10-31 08:54:40 +0300

    Update sshsrv.sh

--- sshsrv.sh
+++ sshsrv.sh
@@ -1,2 +1,5 @@
 #!bin/bash
-scp
+echo NAPISHY PAROL 'P@ssw0rd'
+scp -r /home/user/SH/* user@192.168.100.2:/home/user/SH
+echo NAPISHY PAROL 'resu'
+ssh user@192.168.100.2 "sudo bash /home/user/SH/HQ-SRV.sh"
