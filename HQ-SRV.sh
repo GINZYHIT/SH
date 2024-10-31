a2ffa043418f6a6c67dd1317a1b8adb9c28790af
GINZYHIT <ginzy.hit@gmail.com>
2024-10-31 10:55:20 +0300

    Update HQ-SRV.sh

--- HQ-SRV.sh
+++ HQ-SRV.sh
@@ -4,7 +4,7 @@ echo 192.168.100.2/26 > /etc/net/ifaces/enp6s18/ipv4address
 echo default via 192.168.100.1 > /etc/net/ifaces/enp6s18/ipv4route
 echo nameserver 77.88.8.8 > /etc/net/ifaces/enp6s18/resolv.conf
 systemctl restart network
-sh SH/sshuser.sh
+sh /sshuser.sh
 usermod -aG wheel sshuser
 echo -e "WHEEL_USERS ALL=(ALL:ALL) ALL\nWHEEL_USERS ALL=(ALL:ALL) NOPASSWD: ALL" >> /etc/sudoers
 echo -e "Port 2024\nMaxAuthTries 2\nBanner /etc/mybanner\nAllowUsers sshuser" >> /etc/openssh/sshd_config
